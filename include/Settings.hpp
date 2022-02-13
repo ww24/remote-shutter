376b10dfbd60afdd3be79810372e4f4fbf234a8e
ww24 <ww24@users.noreply.github.com>
2022-02-13 19:21:29 +0900

    chore: follow style guides

--- include/Settings.hpp
+++ include/Settings.hpp
@@ -1,4 +1,14 @@
+// Copyright (c) 2022 Takenori Nakagawa <ww24gm+oss@gmail.com>
+
+#ifndef INCLUDE_SETTINGS_HPP_
+#define INCLUDE_SETTINGS_HPP_
+
+#include <algorithm>
+
 // SETTINGS
 #define DEVICE_NAME "RemoteShutter"
 
-#define BATTERY_PERCENTAGE(voltage) max(min((voltage < 3.2) ? 0 : (voltage - 3.2) * 100, 0.0), 100.0)
+#define BATTERY_PERCENTAGE(voltage) \
+  max(min((voltage < 3.2) ? 0 : (voltage - 3.2) * 100, 0.0), 100.0)
+
+#endif  // INCLUDE_SETTINGS_HPP_
--- src/atom.cpp
+++ src/atom.cpp
@@ -1,57 +1,52 @@
+// Copyright (c) 2022 Takenori Nakagawa <ww24gm+oss@gmail.com>
+
 #if defined(M5_ATOM)
 #include <M5Atom.h>
 
-#include "Settings.hpp"
 #include "RemoteShutter.hpp"
+#include "Settings.hpp"
 
 RemoteShutter rs;
 
 const CRGB disconnectedLED = CRGB(128, 0, 0);
 const CRGB connectedLED = CRGB(0, 128, 0);
 
-void setup()
-{
-    M5.begin(true, false, true);
+void setup() {
+  M5.begin(true, false, true);
 
-    Serial.begin(115200);
-    Serial.println("started");
+  Serial.begin(115200);
+  Serial.println("started");
 
-    rs.setName(DEVICE_NAME);
-    rs.begin();
+  rs.setName(DEVICE_NAME);
+  rs.begin();
 
-    M5.dis.clear();
+  M5.dis.clear();
+  M5.dis.drawpix(0, disconnectedLED);
+}
+
+void loop() {
+  if (rs.isConnected()) {
+    if (rs.wasConnected()) {
+      Serial.println("connected");
+      M5.dis.drawpix(0, connectedLED);
+    }
+
+    if (M5.Btn.wasPressed()) {
+      Serial.println("Release the shutter");
+      rs.releaseShutter(millis());
+    } else if (rs.lastSentDuration(millis()) > 5000) {
+      // about 5 seconds after from last sent
+
+      Serial.println("Keep display on");
+      rs.keepDisplayOn(millis());
+    }
+  } else if (rs.wasDisconnected()) {
+    Serial.println("disconnected");
     M5.dis.drawpix(0, disconnectedLED);
+  }
+
+  delay(100);
+  M5.update();
 }
 
-void loop()
-{
-    if (rs.isConnected())
-    {
-        if (rs.wasConnected())
-        {
-            Serial.println("connected");
-            M5.dis.drawpix(0, connectedLED);
-        }
-
-        if (M5.Btn.wasPressed())
-        {
-            Serial.println("Release the shutter");
-            rs.releaseShutter(millis());
-        }
-        else if (rs.lastSentDuration(millis()) > 5000) // about 5 seconds after from last sent
-        {
-            Serial.println("Keep display on");
-            rs.keepDisplayOn(millis());
-        }
-    }
-    else if (rs.wasDisconnected())
-    {
-        Serial.println("disconnected");
-        M5.dis.drawpix(0, disconnectedLED);
-    }
-
-    delay(100);
-    M5.update();
-}
-
-#endif // M5_ATOM
+#endif  // M5_ATOM
